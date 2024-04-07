c9475873c9beba1d486fdb1ed4d0da55072bfa88
rozannouval <rozannouval@gmail.com>
2024-04-07 13:14:23 +0700

    kok error

--- src/lib/api-libs.js
+++ src/lib/api-libs.js
@@ -1,28 +1,16 @@
-export const getApiData = async (resource, query) => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`);
-  
-  // Memeriksa apakah respons berhasil diterima
-  if (!response.ok) {
-    throw new Error(`Failed to fetch data from ${resource}`);
-  }
-  
-  // Mengonversi respons menjadi objek JSON
-  const apiData = await response.json();
-  
-  // Memeriksa apakah respons memiliki properti 'data'
-  if (!apiData || typeof apiData !== 'object' || !apiData.data) {
-    throw new Error('Invalid API response or missing data');
-  }
-  
-  return apiData;
-};
-
-
-export const getNestedApiData = async (resource, objectProperty) => {
-  const response = await getApiData(resource);
-  return response.data.map(item => item[objectProperty]).reduce((acc, val) => acc.concat(val), []);
+export const getApiData = async(resource, query) => {
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`
+      );
+    
+      const apiData = await response.json();
+    return apiData
 }
 
+export const getNestedApiData = async(resource, objectProperty) => {
+  const response = await getApiData(resource)
+  return response.data.flatMap(item => item[objectProperty])
+}
 
 export const reproduce = (data, gap) => {
   const first = Math.floor(Math.random() * (data.length - gap) + 1)
